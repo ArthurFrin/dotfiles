613faee5a170a258952d7a2bb39ce3e9c9f9a0b2
ArthurFrin <frin.arthur@gmail.com>
2025-11-16 03:22:18 +0100

    fix: update keybinds and installation script for improved functionality

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ SDDM_THEME="chili-sddm-theme"     # ou "sddm-astronaut-theme"
 SDDM_THEME_NAME="chili"           # ou "astronaut"
 
 # --- Paquets ---
-PACMAN_PKGS="hyprland waybar kitty ttf-dejavu nerd-fonts base-devel git stow firefox zsh sddm hyprpaper"
+PACMAN_PKGS="hyprland waybar kitty ttf-dejavu nerd-fonts base-devel git stow firefox zsh sddm hyprpaper vim fastfetch"
 YAY_PKGS="zen-browser-bin $SDDM_THEME bibata-cursor-theme-bin bauh wofi walker elephant elephant-symbols elephant-desktopapplications elephant-calc elephant-websearch elephant-clipboard elephant-files"
 
 echo "$HOME"
@@ -32,7 +32,7 @@ fi
 cd "$HOME/dotfiles"
 
 # --- Stow configs ---
-stow --target="$HOME" bin hypr waybar wallpapers fastfetch kitty zsh wofi
+stow --target="$HOME" bin hypr waybar wallpapers fastfetch kitty zsh walker
 sudo stow --target=/ sddm
 
 # --- Activer SDDM ---
